5660b22f708a548728aefffba1a33f977201ebe8
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-11-18 21:19:55 +0200

    L3_Enemies spawner implemented

---
+++ src/Game_Project1/Assets/Code/Gameplay/EnemiesSpawner.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace Code.Gameplay
+{
+    internal sealed class EnemiesSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private int _spawnInterval =2;
+        
+        [SerializeField]
+        private GameObject _enemy;
+
+        private float _timer;
+        
+        private void Start()
+        {
+            _timer = _spawnInterval;
+        }
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                SpawnEnemy();
+                _timer = _spawnInterval;
+            }
+               
+        }
+
+        private void SpawnEnemy()
+        {
+            Instantiate(_enemy, gameObject.transform);
+        }
+    }
+}
